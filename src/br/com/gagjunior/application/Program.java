1a13e5472b8989c22c23cb3beff43442373c1125
Gilberto Junior <gagjunior@hotmail.com>
2022-10-12 23:09:45 -0300

    Criar entidade Seller

--- src/br/com/gagjunior/application/Program.java
+++ src/br/com/gagjunior/application/Program.java
@@ -1,14 +1,23 @@
 package br.com.gagjunior.application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import br.com.gagjunior.model.entities.Department;
+import br.com.gagjunior.model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
-		Department department = new Department(1, "Teste");
-		System.out.println(department);
+    public static void main(String[] args) {
+	
+	DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	LocalDate birthDate = LocalDate.parse("25/06/1987", sdf);
 
-	}
+	Department department = new Department(1, "Teste");
+	Seller seller = new Seller(1, "Gilberto", "gagjunior@hotmail.com", birthDate, 5000.00, department);
+	System.out.println(department);
+	System.out.println(seller);
+
+    }
 
 }
