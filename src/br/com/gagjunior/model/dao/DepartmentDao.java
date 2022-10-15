54103bc887426647383aaf4f76507864cbed0b49
Gilberto Junior <gagjunior@hotmail.com>
2022-10-14 23:11:04 -0300

    Criar interfaces

--- src/br/com/gagjunior/model/dao/DepartmentDao.java
+++ src/br/com/gagjunior/model/dao/DepartmentDao.java
@@ -1,9 +1,13 @@
 package br.com.gagjunior.model.dao;
 
+import java.util.List;
+
 import br.com.gagjunior.model.entities.Department;
 
 public interface DepartmentDao {
 	void insert(Department department);
 	void update(Department department);
 	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
 }
